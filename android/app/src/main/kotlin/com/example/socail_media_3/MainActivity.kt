1dcccdf63fa3d5f62e233140b6326cccf70c5ed4
GajiHabib <gmhabiburrahman295@gmail.com>
2024-01-05 05:09:07 +0600

    first

---
+++ android/app/src/main/kotlin/com/example/socail_media_3/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.socail_media_3
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/models/post_model.dart
@@ -0,0 +1,22 @@
+import 'package:socail_media_3/models/user_model.dart';
+
+
+class Post {
+  final User user;
+  final String? caption;
+  final String? timeAgo;
+  final String? imageUrl;
+  final int? likes;
+  final int? comments;
+  final int? shares;
+
+  const Post({
+    required this.user,
+    required this.caption,
+    required this.timeAgo,
+    required this.imageUrl,
+    required this.likes,
+    required this.comments,
+    required this.shares,
+  });
+}
